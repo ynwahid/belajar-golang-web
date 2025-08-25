aa73908e37735d451aaf0d7b085845019cbffc51
ynwahid <yusufedu99@gmail.com>
2025-08-25 21:32:05 +0700

    servemux

--- handler_test.go
+++ handler_test.go
@@ -21,3 +21,32 @@ func TestHandler(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, World!")
+	})
+
+	mux.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hi")
+	})
+
+	mux.HandleFunc("/images/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Image")
+	})
+
+	// will prioritize the longest path
+	mux.HandleFunc("/images/thumbnails", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Thumbnail")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
