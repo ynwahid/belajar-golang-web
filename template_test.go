71410bd9c4764a8990d48293ac7ccc4e675b48bd
ynwahid <yusufedu99@gmail.com>
2025-08-27 06:11:32 +0700

    template data struct

--- template_test.go
+++ template_test.go
@@ -86,11 +86,42 @@ func TemplateDataMap(writer http.ResponseWriter, request *http.Request) {
 	})
 }
 
+type Page struct {
+	Title string
+	Name  string
+	Address
+}
+
+type Address struct {
+	Street string
+}
+
+func TemplateDataStruct(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/name.gohtml"))
+	t.ExecuteTemplate(writer, "name.gohtml", Page{
+		Title: "Template Data Struct",
+		Name:  "Ucup",
+		Address: Address{
+			Street: "Jl. Pahlawan",
+		},
+	})
+}
+
 func TestTemplateDataMap(t *testing.T) {
 	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
 	recorder := httptest.NewRecorder()
 
 	TemplateDataMap(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
+
+func TestTemplateDataStruct(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateDataStruct(recorder, request)
 
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
