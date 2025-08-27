a52ad27b0460ad5f8e1912a6f8ea83f337dc3566
ynwahid <yusufedu99@gmail.com>
2025-08-27 07:25:53 +0700

    range template action

--- template_action_test.go
+++ template_action_test.go
@@ -44,3 +44,21 @@ func TestTemplateActionComparison(t *testing.T) {
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
+
+func TemplateActionRange(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/range.gohtml"))
+	t.ExecuteTemplate(writer, "range.gohtml", map[string]any{
+		"Title":   "Template Action Range",
+		"Hobbies": []string{"Reading", "Swimming", "Archery"},
+	})
+}
+
+func TestTemplateActionRange(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionRange(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
