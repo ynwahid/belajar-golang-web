808acb2694538e777b57e629ebee22832f180f1a
ynwahid <yusufedu99@gmail.com>
2025-08-28 05:44:16 +0700

    template layout

---
+++ layout_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TemplateLayout(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles(
+		"./templates/header.gohtml",
+		"./templates/footer.gohtml",
+		"./templates/layout.gohtml",
+	))
+	t.ExecuteTemplate(
+		writer,
+		"layout.gohtml",
+		map[string]any{
+			"Title": "Template Layout",
+			"Name":  "Ucup",
+		},
+	)
+}
+
+func TestTemplateLayout(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateLayout(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
