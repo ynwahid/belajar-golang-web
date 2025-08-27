f4d6420c9da992eccd660cef78c6e0771079ad49
ynwahid <yusufedu99@gmail.com>
2025-08-27 07:13:29 +0700

    comparison operator

--- template_action_test.go
+++ template_action_test.go
@@ -26,3 +26,21 @@ func TestActionIf(t *testing.T) {
 	body, _ := io.ReadAll(recorder.Result().Body)
 	fmt.Println(string(body))
 }
+
+func TemplateActionComparison(writer http.ResponseWriter, request *http.Request) {
+	t := template.Must(template.ParseFiles("./templates/comparison.gohtml"))
+	t.ExecuteTemplate(writer, "comparison.gohtml", map[string]any{
+		"Title":      "Template Action Comparison",
+		"FinalValue": 100,
+	})
+}
+
+func TestTemplateActionComparison(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateActionComparison(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+	fmt.Println(string(body))
+}
