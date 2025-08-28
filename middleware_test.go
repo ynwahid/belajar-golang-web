88907795597a83c282bc880304176ef1285af4f8
ynwahid <yusufedu99@gmail.com>
2025-08-28 21:58:22 +0700

    recover a panic with an error middleware

--- middleware_test.go
+++ middleware_test.go
@@ -6,8 +6,24 @@ import (
 	"testing"
 )
 
+type ErrorMiddleware struct {
+	Handler http.Handler
+}
+
 type LogMiddleware struct {
 	Handler http.Handler
+}
+
+func (middleware *ErrorMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("Terjadi Error")
+			writer.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(writer, "Error: %v", err)
+		}
+	}()
+	middleware.Handler.ServeHTTP(writer, request)
 }
 
 func (middleware *LogMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
@@ -26,12 +42,19 @@ func TestMiddleware(t *testing.T) {
 		fmt.Println("Handler Foo Executed")
 		fmt.Fprint(writer, "Hello Foo")
 	})
+	mux.HandleFunc("/panic", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("Handler Panic Executed")
+		panic("a panic occurred in the system")
+	})
 	logMiddleware := &LogMiddleware{}
 	logMiddleware.Handler = mux
 
+	errorMiddleware := &ErrorMiddleware{}
+	errorMiddleware.Handler = logMiddleware
+
 	server := http.Server{
 		Addr:    "localhost:8080",
-		Handler: logMiddleware,
+		Handler: errorMiddleware,
 	}
 	err := server.ListenAndServe()
 	if err != nil {
